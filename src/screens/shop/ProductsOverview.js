16c034c8fcdf8a612fa8ac131608556b45145bd2
arthurs14 <arthurcs426@gmail.com>
2021-05-23 14:28:05 -0700

    chnaged viwe to flatlist on overview screen and added a model for products

--- src/screens/shop/ProductsOverview.js
+++ src/screens/shop/ProductsOverview.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 
 // Shows the list of all products that will be in our application
 const ProductsOverview = () => {
-  return (
-    <View>
-      <Text>Products Overview Screen</Text>
-    </View>
-  );
+  return <FlatList />;
 };
 
 export default ProductsOverview;
