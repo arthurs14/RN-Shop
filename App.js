2b4275a8e6c7456f72b486038d4c1bab53499f1d
arthurs14 <arthurcs426@gmail.com>
2021-05-23 14:42:19 -0700

    added redux to main app and fixed an issue with product not loading the data in

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+import productsReducer from './src/store/reducers/productsReducer';
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+});
+
+const store = createStore(rootReducer);
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello World!</Text>
-    </View>
+    <Provider store={store}>
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    </Provider>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default App;
--- src/screens/shop/ProductsOverview.js
+++ src/screens/shop/ProductsOverview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { FlatList } from 'react-native';
 
 // Shows the list of all products that will be in our application
 const ProductsOverview = () => {
