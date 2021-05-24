f4de7cc817437dde6babacca2f18b6eb28b8c3fb
arthurs14 <arthurcs426@gmail.com>
2021-05-24 14:54:18 -0700

    First screen set up in Stack Navigator file

--- src/navigation/ShopNavigator.js
+++ src/navigation/ShopNavigator.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+import ProductsOverview from '../screens/shop/ProductsOverview';
+import Colors from '../constants/Colors';
 
 const Stack = createStackNavigator();
 
-const StackNavigator = () => {
-  return;
+const ProductsNavigator = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: Platform.OS === 'andorid' ? Colors.primary : 'white',
+          headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
+        },
+      }}>
+      <Stack.Screen name="Products Overview" component={ProductsOverview} />
+    </Stack.Navigator>
+  );
 };
 
-export default StackNavigator;
+export default ProductsNavigator;
