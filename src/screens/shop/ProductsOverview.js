2bfd77d9e0d1ba2b9193c012973274a98dbfa74e
arthurs14 <arthurcs426@gmail.com>
2021-05-24 15:08:20 -0700

    made it so the actual screen have its own header title

--- src/screens/shop/ProductsOverview.js
+++ src/screens/shop/ProductsOverview.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { FlatList, Text } from 'react-native';
 import { useSelector } from 'react-redux';
 
 // Shows the list of all products that will be in our application
-const ProductsOverview = () => {
+const ProductsOverview = ({ navigation }) => {
   const products = useSelector(state => state.products.availableProducts);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'Products',
+    });
+  }, [navigation]);
 
   return (
     <FlatList
