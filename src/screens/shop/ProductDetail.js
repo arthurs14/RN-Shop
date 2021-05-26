1020adc10dabc3b4a30f8fb5be5106401ffdf88c
arthurs14 <arthurcs426@gmail.com>
2021-05-25 17:18:58 -0700

    added the information that will show when you click on more details of a product

--- src/screens/shop/ProductDetail.js
+++ src/screens/shop/ProductDetail.js
@@ -1,5 +1,12 @@
 import React, { useLayoutEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Image,
+  Button,
+} from 'react-native';
 import { useSelector } from 'react-redux';
 
 // Shows information about one product when selected
@@ -16,9 +23,12 @@ const ProductDetail = ({ navigation, route }) => {
   });
 
   return (
-    <View>
-      <Text>Product Detail Screen</Text>
-    </View>
+    <ScrollView>
+      <Image source={{ uri: product.imageUrl }} />
+      <Button title="Add to Cart" onPress={() => {}} />
+      <Text>{`$${product.price.toFixed(2)}`}</Text>
+      <Text>{product.description}</Text>
+    </ScrollView>
   );
 };
 
