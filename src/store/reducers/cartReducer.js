7be7a3e18c7bd66630aa38ed521ac5f27f695fcf
arthurs14 <arthurcs426@gmail.com>
2021-05-26 17:26:08 -0700

    figured the logic for adding a new cart item to cart object

--- src/store/reducers/cartReducer.js
+++ src/store/reducers/cartReducer.js
@@ -1,4 +1,5 @@
 import { ADD_TO_CART } from '../actions/cartActions';
+import CartItem from '../../models/cart-item';
 
 const initialState = {
   items: {},
@@ -11,6 +12,19 @@ const cartReducer = (state = initialState, action) => {
       const addProduct = action.product;
       const price = addProduct.price;
       const title = addProduct.title;
+
+      if (state.items[addProduct.id]) {
+        // already exists
+      } else {
+        const newCartItem = new CartItem(1, price, title, price);
+        return {
+          ...state,
+          items: {
+            ...state.items,
+            [addProduct.id]: newCartItem,
+          },
+        };
+      }
       break;
   }
   return state;
