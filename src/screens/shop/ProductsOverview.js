c8011d012dbbdfcad8f0a02af91f2fe10af4b7a4
arthurs14 <arthurcs426@gmail.com>
2021-05-31 13:13:43 -0700

    added an icon to the right of header on products overview screen

--- src/screens/shop/ProductsOverview.js
+++ src/screens/shop/ProductsOverview.js
@@ -1,9 +1,11 @@
 import React, { useLayoutEffect } from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, Platform } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import ProductItem from '../../components/shop/ProductItem';
 import * as cartActions from '../../store/actions/cartActions';
+import HeaderButton from '../../components/UI/HeaderButton';
 
 // Shows the list of all products that will be in our application
 const ProductsOverview = ({ navigation }) => {
@@ -13,6 +15,15 @@ const ProductsOverview = ({ navigation }) => {
   useLayoutEffect(() => {
     navigation.setOptions({
       title: 'Products',
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item
+            title="Cart"
+            iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+            onPress={() => {}}
+          />
+        </HeaderButtons>
+      ),
     });
   }, [navigation]);
 
