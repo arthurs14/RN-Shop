7e5d791e5a0f84247d235cede9e92d96b54da8bf
arthurs14 <arthurcs426@gmail.com>
2021-06-01 16:22:28 -0700

    layout for cart screen planned out

--- src/screens/shop/Cart.js
+++ src/screens/shop/Cart.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Button } from 'react-native';
 
 // Shows items that you added to the cart
 const Cart = () => {
   return (
     <View>
-      <Text>Cart Screen</Text>
+      <View>
+        <Text>
+          Total: <Text>${19.99}</Text>
+        </Text>
+        <Button title="Order Now" />
+      </View>
+      <View>
+        <Text>CART ITEMS</Text>
+        {/* Will be FlatList in future */}
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({});
 
 export default Cart;
