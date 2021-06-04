b8885d83da9f4fa62e928931861f17bd8610a80f
arthurs14 <arthurcs426@gmail.com>
2021-06-03 17:04:15 -0700

    added actions and reducer for orders files

---
+++ src/store/reducers/ordersReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  orders: [],
+};
+
+const ordersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+  // return state;
+};
+
+export default ordersReducer;
