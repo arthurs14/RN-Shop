486c64a59bb3008075f09dde2b686887a545aa9f
arthurs14 <arthurcs426@gmail.com>
2021-06-07 17:43:07 -0700

    created a drawer navigator that has our stack shop navigator along with our orders

---
+++ src/navigation/DrawerNavigator.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { screenOptions } from './DefaultNavigationSettings';
+import ShopNavigator from './ShopNavigator';
+import Orders from '../screens/shop/Orders';
+import Colors from '../constants/Colors';
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen name="Orders" component={Orders} />
+    </Stack.Navigator>
+  );
+};
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator screenOptions={screenOptions}>
+      <Drawer.Screen name="Shop" component={ShopNavigator} />
+      <Drawer.Screen name="Orders" component={StackNavigator} />
+    </Drawer.Navigator>
+  );
+};
+
+export default DrawerNavigator;
