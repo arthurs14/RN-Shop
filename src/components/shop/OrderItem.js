daa3f72847bcd8707d8ea2cdb3a9ace238a19e39
arthurs14 <arthurcs426@gmail.com>
2021-06-08 18:18:39 -0700

    fixed an issue with the date breaking the orderitem list

--- src/components/shop/OrderItem.js
+++ src/components/shop/OrderItem.js
@@ -4,12 +4,15 @@ import { View, Text, StyleSheet, Button } from 'react-native';
 import CartItem from './CartItem';
 import Colors from '../../constants/Colors';
 
-const OrderItem = ({ amount, date }) => {
+const OrderItem = ({ itemData }) => {
+  const { totalAmount, readableDate } = itemData;
+  console.log(itemData);
+
   return (
     <View style={styles.orderItem}>
       <View style={styles.summary}>
-        <Text style={styles.totalAmount}>{`$${amount.toFixed(2)}`}</Text>
-        <Text style={styles.date}>{date}</Text>
+        <Text style={styles.totalAmount}>{`$${totalAmount.toFixed(2)}`}</Text>
+        <Text style={styles.date}>{readableDate}</Text>
       </View>
       <Button color={Colors.primary} title="Show Order Details" />
     </View>
--- src/screens/shop/Orders.js
+++ src/screens/shop/Orders.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../../components/UI/HeaderButton';
+import OrderItem from '../../components/shop/OrderItem';
 
 // Shows the list of orders
 const Orders = ({ navigation }) => {
@@ -28,7 +29,7 @@ const Orders = ({ navigation }) => {
     <FlatList
       data={orders}
       keyExtractor={item => item.id}
-      renderItem={itemData => <Text>{itemData.item.totalAmount}</Text>}
+      renderItem={itemData => <OrderItem itemData={itemData.item} />}
     />
   );
 };
