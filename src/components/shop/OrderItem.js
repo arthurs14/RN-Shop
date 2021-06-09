4d79a13660cf74ba7e4e2853377a9922c916ec60
arthurs14 <arthurcs426@gmail.com>
2021-06-08 18:08:12 -0700

    added styling to the order item component

--- src/components/shop/OrderItem.js
+++ src/components/shop/OrderItem.js
@@ -16,6 +16,33 @@ const OrderItem = ({ amount, date }) => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  orderItem: {
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    margin: 20,
+    padding: 10,
+  },
+  summary: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+  },
+  totalAmount: {
+    fontFamily: 'OpenSans-Bold',
+    fontSize: 16,
+  },
+  date: {
+    fontSize: 16,
+    fontFamily: 'OpenSans-Regular',
+    color: '#888',
+  },
+});
 
 export default OrderItem;
