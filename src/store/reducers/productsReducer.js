6a4807e09efc8b08447cfd156673f68d2afdda3f
arthurs14 <arthurcs426@gmail.com>
2021-06-17 18:53:19 -0700

    added reducers for creating and updating a product

--- src/store/reducers/productsReducer.js
+++ src/store/reducers/productsReducer.js
@@ -1,5 +1,10 @@
 import PRODUCTS from '../../data/dummy-data';
-import { DELETE_PRODUCT } from '../actions/productsActions';
+import Product from '../../models/product';
+import {
+  DELETE_PRODUCT,
+  CREATE_PRODUCT,
+  UPDATE_PRODUCT,
+} from '../actions/productsActions';
 
 const initialState = {
   availableProducts: PRODUCTS,
@@ -8,6 +13,50 @@ const initialState = {
 
 const productsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case CREATE_PRODUCT:
+      const newProduct = new Product(
+        new Date().toString(),
+        'u1',
+        action.productData.title,
+        action.productData.imageUrl,
+        action.productData.description,
+        action.productData.price,
+      );
+
+      return {
+        ...state,
+        availableProducts: state.availableProducts.concat(newProduct),
+        userProducts: state.userProducts.concat(newProduct),
+      };
+    case UPDATE_PRODUCT:
+      const { productId } = action;
+      const productIndex = state.userProducts.findIndex(
+        item => item.id === productId,
+      );
+      const availableProductIndex = state.availableProducts.findIndex(
+        item => item.id === productId,
+      );
+
+      const updateProduct = new Product(
+        productId,
+        state.userProducts[productIndex].ownerId,
+        action.productData.title,
+        action.productData.imageUrl,
+        action.productData.description,
+        state.userProducts[productIndex].price,
+      );
+
+      let updateUserProducts = [...state.userProducts];
+      updateUserProducts[productIndex] = updateProduct;
+
+      let updateAvailableProducts = [...state.availableProducts];
+      updateAvailableProducts[availableProductIndex] = updateProduct;
+
+      return {
+        ...state,
+        availableProducts: updateAvailableProducts,
+        userProducts: updateUserProducts,
+      };
     case DELETE_PRODUCT:
       return {
         ...state,
