ce764cbca2e4d6fe4e87e4e9a3277e8dc287da19
arthurs14 <arthurcs426@gmail.com>
2021-06-21 17:41:19 -0700

    optimized some styling to reduce repitition of same code used in multiple files

--- src/components/shop/OrderItem.js
+++ src/components/shop/OrderItem.js
@@ -3,13 +3,14 @@ import { View, Text, StyleSheet, Button } from 'react-native';
 
 import CartItem from '../shop/CartItem';
 import Colors from '../../constants/Colors';
+import Card from '../UI/Card';
 
 const OrderItem = ({ itemData }) => {
   const [showDetails, setShowDetails] = useState(false);
   const { totalAmount, readableDate, items } = itemData;
 
   return (
-    <View style={styles.orderItem}>
+    <Card style={styles.orderItem}>
       <View style={styles.summary}>
         <Text style={styles.totalAmount}>{`$${totalAmount.toFixed(2)}`}</Text>
         <Text style={styles.date}>{readableDate}</Text>
@@ -28,19 +29,12 @@ const OrderItem = ({ itemData }) => {
           ))}
         </View>
       )}
-    </View>
+    </Card>
   );
 };
 
 const styles = StyleSheet.create({
   orderItem: {
-    shadowColor: 'black',
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: 'white',
     margin: 20,
     padding: 10,
     alignItems: 'center',
--- src/screens/shop/Cart.js
+++ src/screens/shop/Cart.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import Colors from '../../constants/Colors';
 import CartItem from '../../components/shop/CartItem';
+import Card from '../../components/UI/Card';
 import * as cartActions from '../../store/actions/cartActions';
 import * as orderActions from '../../store/actions/orderActions';
 
@@ -36,7 +37,7 @@ const Cart = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.summary}>
+      <Card style={styles.summary}>
         <Text style={styles.summaryText}>
           Total: <Text style={styles.amount}>${cartTotal.toFixed(2)}</Text>
         </Text>
@@ -48,7 +49,7 @@ const Cart = ({ navigation }) => {
             dispatch(orderActions.addOrder(cartItems, cartTotal));
           }}
         />
-      </View>
+      </Card>
       <FlatList
         data={cartItems}
         keyExtractor={item => item.id}
@@ -76,13 +77,6 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     marginBottom: 20,
     padding: 10,
-    shadowColor: 'black',
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: 'white',
   },
   summaryText: {
     fontFamily: 'OpenSans-Bold',
